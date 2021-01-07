ddd3680b816a5ce11d0eae3f23146f55009f8b14
Tomi <pintertamas99@gmail.com>
2021-01-07 16:57:00 +0100

    cross platform beeping has been added instead of speaking

--- crypto_price_alert.py
+++ crypto_price_alert.py
@@ -14,6 +14,7 @@ import simpleaudio
 class PriceAlert:
     target = 10000000
     should_check = True
+    should_beep = True
 
     def get_price(self):
         URL = "https://coinmarketcap.com/currencies/ethereum/"
@@ -21,21 +22,14 @@ class PriceAlert:
         soup = BeautifulSoup(page.content, 'html.parser')
 
         result = soup.find(id='__next')
-        price = result.find(class_ = "priceValue___11gHJ")
+        price = result.find(class_="priceValue___11gHJ")
         price = price.text.strip()
         price = self.format_prices(price)
         return price
 
     def play_sound(self):
-        beep(sound=1)
-        #alert_text = "Ethereum price is over {}".format(self.target)
-        #if platform.system() == "Darwin" or platform.system() == "Linux":
-        #    os.system('say {}'.format(alert_text))
-        #elif platform.system() == "Windows":
-        #    import winsound
-        #    duration = 100
-        #    freq = 5000
-        #    winsound.Beep(freq, duration)
+        if self.should_beep:
+            beep(sound=1)
 
     def format_prices(self, price):
         price = Decimal(sub(r'[^\d.]', '', price))
@@ -47,6 +41,12 @@ class PriceAlert:
         try:
             if target == "!p" or target == "!price":
                 print("Current price is: {}".format(self.get_price()))
+            elif target == "!b" or target == "!beep":
+                self.should_beep = not self.should_beep
+                if self.should_beep:
+                    print("Alert sound is on!")
+                else:
+                    print("Alert sound is off!")
             elif target == "!e" or target == "!exit":
                 self.should_check = False
                 print("Exiting...")
@@ -62,6 +62,7 @@ class PriceAlert:
     def check_loop(self):
         while self.should_check:
             self.check_price()
+
 
 pa = PriceAlert()
 func = threading.Thread(target=pa.check_loop)
